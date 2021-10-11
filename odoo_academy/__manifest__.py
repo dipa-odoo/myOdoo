2edfaf46852a29e1be3b8be90f8c07c7825a36b3
dipa-odoo <dipa@odoo.com>
2021-10-11 15:15:42 +0000

    [ADD] demo

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -17,6 +17,6 @@
         
     ],
     'demo': [
-        
+        'demo/academy_demo.xml',
     ],
 }
--- odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -6,3 +6,7 @@ from odoo import models, fields, api
 class Course(models.Model):
     _name = "academy.course"
     _description = "Course Info"
+    
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text(string="Description")
+    level = fields.Selection(string="Level", selection=[('beginner', 'Beginner'),('intermediate','Intermediate'),('advandced','Advandced')])
